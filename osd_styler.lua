51365df90e67b82bc1f5189ad24d999bebd3b9fe
Ren Tatsumoto <tatsu@autistici.org>
2021-04-14 23:57:51 +0300

    add color support to the styler

--- osd_styler.lua
+++ osd_styler.lua
@@ -30,14 +30,6 @@ function OSD:append(s)
     return self
 end
 
-function OSD:bold(s)
-    return self:append(string.format([[{\b1}%s{\b0}]], s))
-end
-
-function OSD:italics(s)
-    return self:append('{\\i1}'):append(s):append('{\\i0}')
-end
-
 function OSD:newline()
     return self:append([[\N]])
 end
@@ -47,15 +39,43 @@ function OSD:tab()
 end
 
 function OSD:size(size)
-    return self:append(string.format([[{\fs%s}]], size))
+    return self:append('{\\fs'):append(size):append('}')
 end
 
 function OSD:align(number)
-    return self:append(string.format([[{\an%s}]], number))
+    return self:append('{\\an'):append(number):append('}')
 end
 
 function OSD:get_text()
     return table.concat(self.text)
+end
+
+function OSD:color(code)
+    return self:append('{\\1c&H')
+               :append(code:sub(5, 6))
+               :append(code:sub(3, 4))
+               :append(code:sub(1, 2))
+               :append('&}')
+end
+
+function OSD:text(text)
+    return self:append(text)
+end
+
+function OSD:bold(s)
+    return self:append('{\\b1}'):append(s):append('{\\b0}')
+end
+
+function OSD:italics(s)
+    return self:append('{\\i1}'):append(s):append('{\\i0}')
+end
+
+function OSD:submenu(text)
+    return self:color('ffe1d0'):bold(text):color('ffffff')
+end
+
+function OSD:item(text)
+    return self:color('fef6dd'):bold(text):color('ffffff')
 end
 
 return OSD
